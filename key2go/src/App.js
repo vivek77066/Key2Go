4ecef7811fa16bcc75e1f8b98089d79e15e05201
Druman Kaneriya <116833054+druman12@users.noreply.github.com>
2025-01-20 21:50:31 +0530

    Components are created

--- key2go/src/App.js
+++ key2go/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import Navbar from './components/Navbar';
+import './CSS/App.css';
+ 
 
 function App() {
   return (
     <>
+    <div className="navbar">
     <h1>Hello this is first change</h1>
+    </div>
+
+    <Navbar/>
+
     </>
   );
 }
---
+++ key2go/src/components/Navbar.js
@@ -0,0 +1,8 @@
+function Navbar(){
+    return (
+        <>
+        <h1 className='x'>This is Navbar</h1> 
+        </>
+    )
+}
+export default Navbar
